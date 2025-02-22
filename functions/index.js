fe837c2a9aef8e2b1fd102993723776fd74d370a
Parker Roth <parkeroth@gmail.com>
2025-02-22 14:12:43 -0600

    Fix all the lint complaints.

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+/* eslint max-len: ["error", { "code": 200 }] */
+
 /**
  * Import function triggers from their respective submodules:
  *
@@ -9,144 +11,195 @@
 
 const functions = require("firebase-functions/v1");
 const logger = require("firebase-functions/logger");
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 const db = admin.firestore();
 
+/**
+ * Sends an email by adding a document to the "mail" collection in the
+ * database.
+ *
+ * @param {string} to - The recipient's email address.
+ * @param {string} subject - The subject of the email.
+ * @param {string} message - The body text of the email.
+ * @return {Promise<void>} A promise that resolves when the email has been
+ * added to the database.
+ */
 async function sendEmail(to, subject, message) {
   await db.collection("mail").add({
     to: to,
     message: {
       subject: subject,
       text: message,
-    }
-  })
+    },
+  });
 }
 
+/**
+ * Retrieves the email notification targets for a given organization.
+ *
+ * @param {string} orgID - The ID of the organization.
+ * @return {Promise<Array<string>>} A promise that resolves to an array of email notification targets.
+ */
 async function getEmailTargets(orgID) {
-  var snapshot = await db.collection("orgs").doc(orgID).get()
-  var org = snapshot.data()
-  return org.notificationSettings?.notificationTargets
+  const snapshot = await db.collection("orgs").doc(orgID).get();
+  const org = snapshot.data();
+  return org.notificationSettings.notificationTargets;
 }
 
+/**
+ * Fetches the request details for a given organization and booking ID.
+ *
+ * @param {string} orgID - The ID of the organization.
+ * @param {string} bookingID - The ID of the booking.
+ * @return {Promise<Object>} A promise that resolves to the request details data.
+ * @throws Will throw an error if there is an issue fetching the details.
+ */
 async function getRequestDetails(orgID, bookingID) {
   try {
-    var snapshot = await db
-      .collection("orgs")
-      .doc(orgID)
-      .collection("request-details")
-      .doc(bookingID)
-      .get()
-    return snapshot.data()
+    const snapshot = await db
+        .collection("orgs")
+        .doc(orgID)
+        .collection("request-details")
+        .doc(bookingID)
+        .get();
+    return snapshot.data();
   } catch (error) {
-    logger.error("Error fetching details for booking: " + bookingID)
-    throw error
+    logger.error("Error fetching details for booking: " + bookingID);
+    throw error;
   }
 }
 
+/**
+ * Sends an email notification to the org owner when a new request is received.
+ *
+ * @param {string} orgID - The ID of the organization.
+ * @param {string} bookingID - The ID of the booking request.
+ * @return {Promise<void>} - A promise that resolves when the email has been sent.
+ */
 async function notifyOwnerOfPendingBooking(orgID, bookingID) {
   try {
-    var targets = await getEmailTargets(orgID)
-    var target = targets?.bookingCreated ?? ""
-    if (target != "") {
+    const targets = await getEmailTargets(orgID);
+    const target = targets.bookingCreated;
+    if (target != null && target != "") {
       await sendEmail(
-        target,
-        'Booking Request Received', `
-        A new booking request is ready for review.
-        `
-      )
-      logger.debug(`Sent email notification for ${bookingID} to org owner ${target}`)
+          target,
+          "Booking Request Received", `
+          A new booking request is ready for review.
+          `);
+      logger.debug(`Sent email notification for ${bookingID} to org owner ${target}`);
     }
   } catch (error) {
-    logger.error("Error reading target for orgID: " + orgID)
+    logger.error("Error reading target for orgID: " + orgID);
   }
 }
 
+/**
+ * Sends an email notification to the requester when their booking request is received.
+ *
+ * @param {string} orgID - The ID of the organization.
+ * @param {string} bookingID - The ID of the booking request.
+ * @return {Promise<void>} - A promise that resolves when the email has been sent.
+ */
 async function notifyRequesterOfPeningBooking(orgID, bookingID) {
   try {
-    const details =  await getRequestDetails(orgID, bookingID)
+    const details = await getRequestDetails(orgID, bookingID);
     await sendEmail(
-      details.email,
-      'Booking Request Received', `
-      Dear ${details.name},
+        details.email,
+        "Booking Request Received", `
+        Dear ${details.name},
 
-      Thank you for your request for ${details.eventName} has been received and we will be in touch shortly.
+        Thank you for your request for ${details.eventName} has been received and we will be in touch shortly.
 
-      Best,
-      RoomBooker
-      `)
-    logger.debug(`Sent email notification for ${bookingID} to requester ${details.email}`)
+        Best,
+        RoomBooker
+        `);
+    logger.debug(`Sent email notification for ${bookingID} to requester ${details.email}`);
   } catch (error) {
-    logger.error(`Error sending email for ${bookingID}: ${error}`)
+    logger.error(`Error sending email for ${bookingID}: ${error}`);
   }
 }
 
+/**
+ * Sends an email notification to the requester when their booking request is approved.
+ *
+ * @param {string} orgID - The ID of the organization.
+ * @param {string} bookingID - The ID of the booking request.
+ * @return {Promise<void>} - A promise that resolves when the email has been sent.
+ */
 async function notifyRequesterOfBookingApproval(orgID, bookingID) {
   try {
-    const details =  await getRequestDetails(orgID, bookingID)
+    const details = await getRequestDetails(orgID, bookingID);
     await sendEmail(
-      details.email,
-      'Booking Request Approved', `
-      Dear ${details.name},
+        details.email,
+        "Booking Request Approved", `
+        Dear ${details.name},
 
-      Your room booking request for ${details.eventName} has been Approved!
+        Your room booking request for ${details.eventName} has been Approved!
 
-      Best,
-      RoomBooker
-      `)
-    logger.debug(`Sent email notification for ${bookingID} to requester ${details.email}`)
+        Best,
+        RoomBooker
+        `);
+    logger.debug(`Sent email notification for ${bookingID} to requester ${details.email}`);
   } catch (error) {
-    logger.error(`Error sending email for ${bookingID}: ${error}`)
+    logger.error(`Error sending email for ${bookingID}: ${error}`);
   }
 }
 
+/**
+ * Sends an email notification to the requester when their booking request is denied.
+ *
+ * @param {string} orgID - The ID of the organization.
+ * @param {string} bookingID - The ID of the booking request.
+ * @return {Promise<void>} - A promise that resolves when the email has been sent.
+ */
 async function notifyRequesterOfBookingDenial(orgID, bookingID) {
   try {
-    const details =  await getRequestDetails(orgID, bookingID)
+    const details = await getRequestDetails(orgID, bookingID);
     await sendEmail(
-      details.email,
-      'Booking Request Denied', `
-      Dear ${details.name},
+        details.email,
+        "Booking Request Denied", `
+        Dear ${details.name},
 
-      Unfortunately rour room booking request for ${details.eventName} has been denied.
+        Unfortunately rour room booking request for ${details.eventName} has been denied.
 
-      Best,
-      RoomBooker
-      `)
-    logger.debug(`Sent email notification for ${bookingID} to requester ${details.email}`)
+        Best,
+        RoomBooker
+        `);
+    logger.debug(`Sent email notification for ${bookingID} to requester ${details.email}`);
   } catch (error) {
-    logger.error(`Error sending email for ${bookingID}: ${error}`)
+    logger.error(`Error sending email for ${bookingID}: ${error}`);
   }
 }
 
 exports.onNewPendingBooking = functions.firestore
     .document("orgs/{orgID}/pending-requests/{bookingID}")
     .onCreate(async (snap, context) => {
-      const orgID = context.params.orgID
-      const bookingID = context.params.bookingID
-      logger.log(`Received new booking request (${bookingID})`)
-      await notifyRequesterOfPeningBooking(orgID, bookingID)
-      await notifyOwnerOfPendingBooking(orgID, bookingID)
+      const orgID = context.params.orgID;
+      const bookingID = context.params.bookingID;
+      logger.log(`Received new booking request (${bookingID})`);
+      await notifyRequesterOfPeningBooking(orgID, bookingID);
+      await notifyOwnerOfPendingBooking(orgID, bookingID);
       logger.log(`Function finished for request ${bookingID}`);
     });
 
 exports.onRequestApproved = functions.firestore
     .document("orgs/{orgID}/confirmed-requests/{bookingID}")
     .onCreate(async (snap, context) => {
-      const orgID = context.params.orgID
-      const bookingID = context.params.bookingID
-      logger.log(`Received new request approval (${bookingID})`)
-      await notifyRequesterOfBookingApproval(orgID, bookingID)
+      const orgID = context.params.orgID;
+      const bookingID = context.params.bookingID;
+      logger.log(`Received new request approval (${bookingID})`);
+      await notifyRequesterOfBookingApproval(orgID, bookingID);
       logger.log(`Function finished for approval of ${bookingID}`);
     });
 
 exports.onRequestDenied = functions.firestore
     .document("orgs/{orgID}/denied-requests/{bookingID}")
     .onCreate(async (snap, context) => {
-      const orgID = context.params.orgID
-      const bookingID = context.params.bookingID
-      logger.log(`Received new request denial (${bookingID})`)
-      await notifyRequesterOfBookingDenial(orgID, bookingID)
+      const orgID = context.params.orgID;
+      const bookingID = context.params.bookingID;
+      logger.log(`Received new request denial (${bookingID})`);
+      await notifyRequesterOfBookingDenial(orgID, bookingID);
       logger.log(`Function finished for denial of ${bookingID}`);
     });
