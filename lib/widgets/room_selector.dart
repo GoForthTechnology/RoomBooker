807e642be26f663e8a4d2b737e754bc05ab50c6b
Parker Roth <parkeroth@gmail.com>
2025-03-08 14:24:42 -0600

    Update room colors.

--- lib/widgets/room_selector.dart
+++ lib/widgets/room_selector.dart
@@ -4,13 +4,20 @@ import 'package:provider/provider.dart';
 import 'package:room_booker/entities/organization.dart';
 import 'package:room_booker/repos/org_repo.dart';
 
+List<Color> lightColors = [];
+
 List<Color> roomColors = [
-  Colors.blueAccent,
-  Colors.redAccent,
-  Colors.greenAccent,
-  Colors.purpleAccent,
-  Colors.orangeAccent,
-  Colors.yellowAccent,
+  Color.fromRGBO(131, 45, 164, 1),
+  Color.fromRGBO(123, 134, 198, 1),
+  Color.fromRGBO(67, 80, 175, 1),
+  Color.fromRGBO(69, 153, 223, 1),
+  Color.fromRGBO(57, 126, 73, 1),
+  Color.fromRGBO(93, 179, 126, 1),
+  Color.fromRGBO(237, 193, 75, 1),
+  Color.fromRGBO(226, 93, 51, 1),
+  Color.fromRGBO(216, 129, 119, 1),
+  Color.fromRGBO(195, 41, 28, 1),
+  Color.fromRGBO(97, 97, 97, 1),
 ];
 
 class RoomState extends ChangeNotifier {
