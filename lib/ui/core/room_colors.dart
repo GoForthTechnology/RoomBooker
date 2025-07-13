d2f896d01dca39caae93b307d83548debdab3d39
Parker Roth <parkeroth@gmail.com>
2025-07-13 14:29:00 -0500

    Add more color options.

--- lib/ui/core/room_colors.dart
+++ lib/ui/core/room_colors.dart
@@ -1,17 +1,29 @@
 import 'dart:ui';
 
 const List<Color> colors = [
+  Color.fromRGBO(165, 155, 143, 1),
+  Color.fromRGBO(97, 97, 97, 1),
   Color.fromRGBO(131, 45, 164, 1),
+  Color.fromRGBO(150, 107, 171, 1),
+  Color.fromRGBO(175, 158, 215, 1),
   Color.fromRGBO(123, 134, 198, 1),
   Color.fromRGBO(67, 80, 175, 1),
+  Color.fromRGBO(83, 131, 236, 1),
   Color.fromRGBO(69, 153, 223, 1),
+  Color.fromRGBO(66, 148, 136, 1),
   Color.fromRGBO(57, 126, 73, 1),
   Color.fromRGBO(93, 179, 126, 1),
+  Color.fromRGBO(136, 178, 83, 1),
+  Color.fromRGBO(194, 202, 81, 1),
+  Color.fromRGBO(223, 197, 90, 1),
   Color.fromRGBO(237, 193, 75, 1),
+  Color.fromRGBO(227, 151, 53, 1),
+  Color.fromRGBO(223, 116, 44, 1),
   Color.fromRGBO(226, 93, 51, 1),
   Color.fromRGBO(216, 129, 119, 1),
   Color.fromRGBO(195, 41, 28, 1),
-  Color.fromRGBO(97, 97, 97, 1),
+  Color.fromRGBO(198, 51, 97, 1),
+  Color.fromRGBO(159, 39, 87, 1),
 ];
 
 Color? fromHex(String? hexColor) {
