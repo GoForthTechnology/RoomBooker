ab14458eb753222adf9131f0cba065772d10d568
Parker Roth <parkeroth@gmail.com>
2025-07-18 10:56:42 -0500

    Capture screenshots on errors

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,7 @@ void main() async {
         // The sampling rate for profiling is relative to tracesSampleRate
         // Setting to 1.0 will profile 100% of sampled transactions:
         options.profilesSampleRate = 1.0;
+        options.attachScreenshot = true;
       },
       appRunner: () => runApp(SentryWidget(child: MyApp())),
     );
