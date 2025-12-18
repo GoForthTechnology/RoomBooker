e86e7bbc32f640805f534a7e2c95f391a2260e89
Parker Roth <parkeroth@gmail.com>
2025-12-18 14:07:13 -0600

    Fix typo in start emulators script.

--- scripts/start_emulators.sh
+++ scripts/start_emulators.sh
@@ -4,4 +4,4 @@ DATA_DIR="emulators_data"
 
 echo "Starting Emulators"
 
-fireabase emulators:start --import $DATA_DIR --export-on-exit $DATA_DIR
+firebase emulators:start --import $DATA_DIR --export-on-exit $DATA_DIR
